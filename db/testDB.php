3c92089d7c9cf88092211687176dcadd5855fdb7
Wclement <clement@weinreich.fr>
2021-03-09 12:06:57 +0100

    Test de la base de donnée : fonctionnelle

--- db/testDB.php
+++ db/testDB.php
@@ -1,7 +1,7 @@
 <?php
 
 try{
-    $BDD = new PDO("mysql:host=localhost;dbname=testWebUsers;charset=utf8","user","pass",array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+    $BDD = new PDO("mysql:host=localhost;dbname=annuaireEleves;charset=utf8","annuaireUser","explodingkittens",array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
     // connexion serveur de Base de données MySQL et choix de base
 }catch(Exception $e){
     die('Erreur fatale' . $e->getMessage());
@@ -9,23 +9,41 @@ try{
 
 if ($BDD) {
 
-    $querySettings = $BDD->prepare("SELECT * FROM Settings WHERE IdEleve=1");
-    $querySettings->execute();
+    $id = 1;
+    $querySettings = $BDD->prepare("SELECT * FROM Parametres WHERE IdEleve=?");
+    $querySettings->execute(array($id));
+ 
+    $queryInfosPerso = $BDD->prepare("SELECT * FROM InfosPerso WHERE IdEleve=?");
+    $queryInfosPerso->execute(array($id));
+    $infos = $queryInfosPerso->fetch(); //fetch seul car un seul resultat
 
-    // Normalement c'est ça : 
-        //$queryInfoPerso = $BDD->prepare("SELECT * FROM InfosPerso WHERE IdEleve=?");
-        // mais pas envie de recréer la bdd pour l'instant donc je fais un équivalent mais ce sera pareil 
-    $queryInfosPerso = $BDD->prepare("SELECT * FROM InfosPerso WHERE IdInfosPersos=1");
-    $queryInfosPerso->execute();
-    $infos = $queryInfosPerso->fetch();
+    $queryExperiencePro = $BDD->prepare("SELECT * FROM ExperiencePro WHERE IdEleve=?");
+    $queryExperiencePro->execute(array($id));
+    $experiences = $queryExperiencePro->fetchAll();
+
+
+
+
 
 
     while($tuple= $querySettings -> fetch()){
-        $nomParam = $tuple["nomParam"];
-        $valeur = $infos[$nomParam];
-        $visibilite = $tuple["visibilite"];
+        $nomParam = $tuple["LibelleInformation"];
+        if(is_numeric($nomParam)){
 
-        echo "Nomparam : " . $nomParam . " --- valeur : " . $valeur . " --- visibilite : " . $visibilite . "<br/>";
+            foreach($experiences as $experience){
+                if($experience["IdExperiencePro"]==(int)$nomParam){
+                    $valeur = $experience["TypeExperiencePro"] . " " .$experience["Lieu"];
+                    break;
+                }
+            }
+            
+        }else{
+            $valeur = $infos[$nomParam];
+        }
+        $visibilite = $tuple["Visibilite"];
+        
+
+        echo "LibelleInformation : " . $nomParam . " --- valeur : " . $valeur . " --- visibilite : " . $visibilite . "<br/>";
     }
 
 

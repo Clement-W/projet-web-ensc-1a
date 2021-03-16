d09eb698da2b11847eeb5ab10d7928e6369e82ca
Wclement <clement@weinreich.fr>
2021-03-16 10:39:20 +0100

    modification header

--- includes/fragments/head.php
+++ includes/fragments/head.php
@@ -1,9 +1,9 @@
 <head>
-    <meta charset="UTF-8">
-    <title>CodePen - Pure CSS Gradient Background Animation</title>
-
-    <link rel="stylesheet" href="../css/styles.css"/>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="../css/styles.css" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-
+    <title>Annuaire</title>
 </head>

857910f73ef5cdf05f7ba583567536ac13ac32a1
Wclement <clement@weinreich.fr>
2021-03-27 20:14:36 +0100

    Modification de la modal de changement de mdp pour etre plus générale

--- includes/modals/modifierMotDePasse.php
+++ includes/modals/modifierMotDePasse.php
@@ -10,28 +10,34 @@
             </div>
 
             <!-- Modal body -->
-            <form method="POST" action="modifierProfil.php?idEleve=<?= getIdEleveParNomUtilisateur($_SESSION["nomUtilisateur"]); ?>" class="register-form ml-5 mr-5" id="register-form">
+            <?php if (estGestionnaire()) { ?>
+                <form method="POST" action="accueil.php" class="register-form ml-5 mr-5" id="register-form">
 
-                <div class="form-group">
-                    <label for="ancienMotDePasse"><i class="zmdi zmdi-lock"></i></label>
-                    <input type="password" maxlength="50" name="ancienMotDePasse" id="ancienMotDePasse" placeholder="Ancien mot de passe" required />
-                </div>
-                <div class="form-group">
-                    <label for="nouveauMotDePasse"><i class="zmdi zmdi-account material-icons-name "></i></label>
-                    <input type="password" maxlength="50" name="nouveauMotDePasse" id="nouveauMotDePasse" placeholder="Nouveau mot de passe" required />
-                </div>
-                <div class="form-group">
-                    <label for="confirmeNouveauMotDePasse"><i class="zmdi zmdi-account material-icons-name "></i></label>
-                    <input type="password" maxlength="50" name="confirmeNouveauMotDePasse" id="confirmeNouveauMotDePasse" placeholder="Confirmer le nouveau mot de passe" required />
-                </div>
+                <?php } else { ?>
 
-                <!-- Modal footer -->
-                <div class="modal-footer">
-                    <div class="form-group form-button d-flex ">
-                        <input type="submit" class="btn btn-outline-success" name="modifierMotDePasse" id="modifierMotDePasse" value="Modifier" />
+                    <form method="POST" action="modifierProfil.php?idEleve=<?= getIdEleveParNomUtilisateur($_SESSION["nomUtilisateur"]); ?>" class="register-form ml-5 mr-5" id="register-form">
+
+                    <?php } ?>
+                    <div class="form-group">
+                        <label for="ancienMotDePasse"><i class="zmdi zmdi-lock"></i></label>
+                        <input type="password" maxlength="50" name="ancienMotDePasse" id="ancienMotDePasse" placeholder="Ancien mot de passe" required />
                     </div>
-                </div>
-            </form>
+                    <div class="form-group">
+                        <label for="nouveauMotDePasse"><i class="zmdi zmdi-account material-icons-name "></i></label>
+                        <input type="password" maxlength="50" name="nouveauMotDePasse" id="nouveauMotDePasse" placeholder="Nouveau mot de passe" required />
+                    </div>
+                    <div class="form-group">
+                        <label for="confirmeNouveauMotDePasse"><i class="zmdi zmdi-account material-icons-name "></i></label>
+                        <input type="password" maxlength="50" name="confirmeNouveauMotDePasse" id="confirmeNouveauMotDePasse" placeholder="Confirmer le nouveau mot de passe" required />
+                    </div>
+
+                    <!-- Modal footer -->
+                    <div class="modal-footer">
+                        <div class="form-group form-button d-flex ">
+                            <input type="submit" class="btn btn-outline-success" name="modifierMotDePasse" id="modifierMotDePasse" value="Modifier" />
+                        </div>
+                    </div>
+                    </form>
         </div>
     </div>
 </div>

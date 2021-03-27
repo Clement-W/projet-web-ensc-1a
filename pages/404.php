c115c0955f4e8a93cd1fffa18807b5d7721edc92
Amerix <emma.marqueton@gmail.com>
2021-03-27 01:47:50 +0100

    problème bouton outside container dans profil réglé

--- pages/404.php
+++ pages/404.php
@@ -15,14 +15,11 @@ require_once "../includes/fragments/head.php";
 <body class="background">
     <?php require_once "../includes/fragments/header.php"; ?>
 
-    <div class="d-flex justify-content-center container h-100">
+    <div class="d-flex justify-content-center h-100">
 
         <div class="align-self-center mb-5 text-center">
             <h1><strong>Erreur 404 : </strong> La page demandée n'est pas disponible :'(</h1>
             <h3><a href="accueil.php">Retour vers la page d'accueil</a></h3>
         </div>
-
-
-
     </div>
 </body>
--- pages/modifierProfil.php
+++ pages/modifierProfil.php
@@ -10,17 +10,17 @@ if (!empty($_POST["ajouterExperiencePro"])) {
 //Si on appuie sur le bouton submit du form de mise à jour du profil
 if (!empty($_POST["mettreAJourProfil"])) {
     mettreAJourProfil();
-} 
+}
 //Si on appuie sur le bouton submit de la modal Modifier le mot de passe
 if (!empty($_POST["modifierMotDePasse"])) {
     mettreAJourMotDePasse();
-} 
+}
 
 if (!estConnecte() || empty($_GET["idEleve"]) || !idEleveValide(escape($_GET["idEleve"]) || !(getIdEleveParNomUtilisateur($_SESSION["nomUtilisateur"]) == $_GET["idEleve"]))) {
     // Si un utilisateur n'est pas connecté, ou que l'id eleve n'est pas valide ou pas donné en get alors on redirige vers 404 error
     redirect("404.php");
 } else {
-    
+
     $idEleve = escape($_GET["idEleve"]);
     $infos = getInfosCompteEleveParId($idEleve);
     $nom = $infos["Nom"];
@@ -34,7 +34,7 @@ if (!estConnecte() || empty($_GET["idEleve"]) || !idEleveValide(escape($_GET["id
     $tel = $infos["NumTelephone"];
 
     $experiencePro = getExperiencesProParId($idEleve);
-    $visibilite = getVisibiliteInfosProfil($idEleve); 
+    $visibilite = getVisibiliteInfosProfil($idEleve);
 
 ?>
 
@@ -54,15 +54,17 @@ if (!estConnecte() || empty($_GET["idEleve"]) || !idEleveValide(escape($_GET["id
 
 
             <div class="whitecontainer">
-                <div class="d-flex justify-content-between pt-3">
 
+                <div class="d-flex justify-content-between pt-3">
                     <h2 class="ml-5 ">Modifier votre profil</h2>
 
-                    <div class="d-flex">
+                    <div class="d-flex flex-wrap">
 
                         <!-- Bouton d'ajout d'expérience -->
                         <?php require_once('../includes/modals/ajouterExperience.php'); ?>
-                        <button type="button" id="boutonAjouterExperience" class="btn btn-outline-primary mr-5">Ajouter une expérience</button>
+                        <div>
+                            <button type="button" id="boutonAjouterExperience" class="btn btn-outline-primary mr-5">Ajouter une expérience</button>
+                        </div>
                         <script type="text/javascript">
                             $('#boutonAjouterExperience').on('click', function() {
                                 $('#ajouterExperience').modal('show');
@@ -71,14 +73,14 @@ if (!estConnecte() || empty($_GET["idEleve"]) || !idEleveValide(escape($_GET["id
 
                         <!-- Bouton de modification de mot de passe -->
                         <?php require_once('../includes/modals/modifierMotDePasse.php'); ?>
-                        <button type="button" id="boutonModifierMotDePasse" class="btn btn-outline-danger mr-5">Modifier le mot de passe</button>
+                        <div>
+                            <button type="button" id="boutonModifierMotDePasse" class="btn btn-outline-danger mr-5">Modifier le mot de passe</button>
+                        </div>
                         <script type="text/javascript">
                             $('#boutonModifierMotDePasse').on('click', function() {
                                 $('#modifierMotDePasse').modal('show');
                             });
                         </script>
-
-                        
                     </div>
                 </div>
 
@@ -110,20 +112,20 @@ if (!estConnecte() || empty($_GET["idEleve"]) || !idEleveValide(escape($_GET["id
                         </div>
                         <div class="d-flex">
                             <div class="custom-control custom-checkbox col-sm-3">
-                                <input type="checkbox" name="GenreVisibilite" class="custom-control-input" id="genreVisibilite" <?=($visibilite["Genre"] == true) ? "checked" : "" ?>>
+                                <input type="checkbox" name="GenreVisibilite" class="custom-control-input" id="genreVisibilite" <?= ($visibilite["Genre"] == true) ? "checked" : "" ?>>
                                 <label class="custom-control-label" for="genreVisibilite">Genre</label>
                             </div>
                             <div class="form-group col-sm-8">
                                 <div class="custom-control custom-radio custom-control-inline">
-                                    <input type="radio" class="custom-control-input" id="defaultInline1" name="Genre" value="Masculin" required <?=($genre == "Masculin") ? "checked" : "" ?> >
+                                    <input type="radio" class="custom-control-input" id="defaultInline1" name="Genre" value="Masculin" required <?= ($genre == "Masculin") ? "checked" : "" ?>>
                                     <label class="custom-control-label text-secondary" for="defaultInline1">Masculin</label>
                                 </div>
                                 <div class="custom-control custom-radio custom-control-inline">
-                                    <input type="radio" class="custom-control-input" id="defaultInline2" name="Genre" value="Féminin" <?=($genre == "Féminin") ? "checked" : "" ?>>
+                                    <input type="radio" class="custom-control-input" id="defaultInline2" name="Genre" value="Féminin" <?= ($genre == "Féminin") ? "checked" : "" ?>>
                                     <label class="custom-control-label text-secondary" for="defaultInline2">Féminin</label>
                                 </div>
                                 <div class="custom-control custom-radio custom-control-inline">
-                                    <input type="radio" class="custom-control-input" id="defaultInline3" name="Genre" value="Autre" <?=($genre == "Autre") ? "checked" : "" ?>>
+                                    <input type="radio" class="custom-control-input" id="defaultInline3" name="Genre" value="Autre" <?= ($genre == "Autre") ? "checked" : "" ?>>
                                     <label class="custom-control-label text-secondary" for="defaultInline3">Autre</label>
                                 </div>
                             </div>
@@ -138,7 +140,7 @@ if (!estConnecte() || empty($_GET["idEleve"]) || !idEleveValide(escape($_GET["id
                         </div>
                         <div class="d-flex">
                             <div class="custom-control custom-checkbox mt-2 col-sm-3">
-                                <input type="checkbox" name="AdresseVisibilite" class="custom-control-input" id="adresseVisibilite" <?=($visibilite["Adresse"] == true) ? "checked" : "" ?>  > <!-- on coche si l'utilisateur a ce parametre en visible ou non -->
+                                <input type="checkbox" name="AdresseVisibilite" class="custom-control-input" id="adresseVisibilite" <?= ($visibilite["Adresse"] == true) ? "checked" : "" ?>> <!-- on coche si l'utilisateur a ce parametre en visible ou non -->
                                 <label class="custom-control-label" for="adresseVisibilite">Adresse</label>
                             </div>
                             <div class="form-group col-sm-8">
@@ -147,7 +149,7 @@ if (!estConnecte() || empty($_GET["idEleve"]) || !idEleveValide(escape($_GET["id
                         </div>
                         <div class="d-flex">
                             <div class="custom-control custom-checkbox mt-2 col-sm-3">
-                                <input type="checkbox" name="VilleVisibilite" class="custom-control-input" id="villeVisibilite" <?=($visibilite["Ville"] == true) ? "checked" : "" ?>>
+                                <input type="checkbox" name="VilleVisibilite" class="custom-control-input" id="villeVisibilite" <?= ($visibilite["Ville"] == true) ? "checked" : "" ?>>
                                 <label class="custom-control-label" for="villeVisibilite">Ville</label>
                             </div>
                             <div class="form-group col-sm-8">
@@ -156,7 +158,7 @@ if (!estConnecte() || empty($_GET["idEleve"]) || !idEleveValide(escape($_GET["id
                         </div>
                         <div class="d-flex">
                             <div class="custom-control custom-checkbox mt-2 col-sm-3">
-                                <input type="checkbox" name="CodePostalVisibilite" class="custom-control-input" id="codePostalVisibilite" <?=($visibilite["CodePostal"] == true) ? "checked" : "" ?> >
+                                <input type="checkbox" name="CodePostalVisibilite" class="custom-control-input" id="codePostalVisibilite" <?= ($visibilite["CodePostal"] == true) ? "checked" : "" ?>>
                                 <label class="custom-control-label" for="codePostalVisibilite">Code Postal</label>
                             </div>
                             <div class="form-group col-sm-8">
@@ -166,7 +168,7 @@ if (!estConnecte() || empty($_GET["idEleve"]) || !idEleveValide(escape($_GET["id
 
                         <div class="d-flex">
                             <div class="custom-control custom-checkbox mt-2 col-sm-3">
-                                <input type="checkbox" name="AdresseMailVisibilite" class="custom-control-input" id="adresseMailVisibilite" <?=($visibilite["AdresseMail"] == true) ? "checked" : "" ?> >
+                                <input type="checkbox" name="AdresseMailVisibilite" class="custom-control-input" id="adresseMailVisibilite" <?= ($visibilite["AdresseMail"] == true) ? "checked" : "" ?>>
                                 <label class="custom-control-label" for="adresseMailVisibilite">Adresse Mail</label>
                             </div>
                             <div class="form-group col-sm-8">
@@ -176,7 +178,7 @@ if (!estConnecte() || empty($_GET["idEleve"]) || !idEleveValide(escape($_GET["id
 
                         <div class="d-flex">
                             <div class="custom-control custom-checkbox mt-2 col-sm-3">
-                                <input type="checkbox" name="NumTelephoneVisibilite" class="custom-control-input" id="numTelephoneVisibilite" <?=($visibilite["NumTelephone"] == true) ? "checked" : "" ?> >
+                                <input type="checkbox" name="NumTelephoneVisibilite" class="custom-control-input" id="numTelephoneVisibilite" <?= ($visibilite["NumTelephone"] == true) ? "checked" : "" ?>>
                                 <label class="custom-control-label" for="numTelephoneVisibilite">Téléphone</label>
                             </div>
                             <div class="form-group col-sm-8">
@@ -185,10 +187,9 @@ if (!estConnecte() || empty($_GET["idEleve"]) || !idEleveValide(escape($_GET["id
                         </div>
 
                         </br>
-                        <hr class=" mr-5" />
+                        <hr class="mr-5" />
                         <div class="d-flex justify-content-between pt-3">
                             <p class="h3 text-secondary"><u>Expériences professionnelles</u></p>
-
                         </div>
 
                         </br>
